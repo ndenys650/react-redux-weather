4a09d36f7828dc0981962e8c9a28a41d82e7bdb1
Nate Denys <nate_denys@yahoo.com>
2017-09-20 00:03:24 -0700

    configured callback from input tag
    binding of this to replace and override existing function, defined the input change function, added value to search bar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
 
-// created the container Searchbar component
+// created container Searchbar component
 export default class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = { term: '' };
+
+		// bind the context of onInputChange
+		// take existing function, bind it to 'this', replace existing function
+		// overriding the local method
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+
+	// define input change function
+	onInputChange(event) {
+		console.log(event.target.value);
+		this.setState({ term: event.target.value });
 	}
 
 	render() {
 		return (
 			// created search bar and button with bootstrap classes
 			<form className="input-group">
-				<input />
+				<input 
+					placeholder="Get a five-day forecast in your favorite cities"
+					className="form-control"
+					value={this.state.term}
+					onChange={this.onInputChange}
+				/>
 				<span className="input-group-btn">
 					<button type="submit" className="btn btn-secondary">Submit</button>
 				</span>
